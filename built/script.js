becabdf821eb3e6b7dcc8a8aadef307954ff7677
tcaa-web <kfry@techcollege.dk>
2024-01-08 10:09:05 +0100

    initial commit

---
+++ built/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let firstName = "Kasper"; // String
+let age = 22; // Number
+let isWideScreen = false; // boolean
+let todaysDate = new Date();
+let nullValue = null;
+let undefinedValue = undefined;
+console.log('Hello, my name is: ', firstName, ' and im ', age, ' old');
---
+++ src/script.ts
@@ -0,0 +1,9 @@
+let firstName: string = "Kasper"; // String
+let age: number = 22 // Number
+let isWideScreen: boolean = false; // boolean
+let todaysDate: Date = new Date();
+let nullValue: null = null;
+let undefinedValue: undefined = undefined;
+
+
+console.log('Hello, my name is: ', firstName, ' and im ', age, ' old');
